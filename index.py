dc22cdce2fe686e8e9b8524782176c2c3b8312ca
Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
2024-11-04 13:47:32 +0100

    feat: implemented error logging method
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>

--- index.py
+++ index.py
@@ -1,3 +1,7 @@
+import traceback
+import requests
+import logging
+from datetime import datetime
 from flask import Flask
 
 class Flytrap:
@@ -10,6 +14,34 @@ class Flytrap:
     def setup_flask_error_handler(self, app):
         @app.errorhandler(Exception)
         def handle_exception(error):
-            self._log_error(error)
+            self._log_error(error, False)
             # raise error # Pass the error on to any other error handlers the user might have?
-            
+
+    def _log_error(self, e, handled):
+        if not e:
+            return
+        
+        data = {
+            'error': {
+                'name': type(e).__name__,
+                'message': str(e),
+                'stack': traceback.format_exc(),
+            },
+            'timestamp': datetime.now().astimezone(),
+            'handled': handled,
+            'project_id': self.project_id
+        }
+
+        headers = {
+            'Content-Type': 'application/json',
+            'x-api-key': self.api_key,
+        }
+
+        try:
+            self.logger.info("[flytrap] Sending error to the backend...")
+            response = requests.post(self.api_endpoint, json={"data": data}, headers=headers)
+            response.raise_for_status()
+            self.logger.info("[flytrap]", response.status_code, response.text)
+        except requests.RequestException as e:
+            self.logger.error("[flytrap] An error occurred sending error data: %s", e)
+            # Raise a new FlytrapError?
