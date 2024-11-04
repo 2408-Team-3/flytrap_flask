5dea2f16e61727cb62bb93be8e3f5f4a657e4960
Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
2024-11-04 13:37:35 +0100

    feat: added error handler for all Flask routes
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>

--- index.py
+++ index.py
@@ -1,9 +1,15 @@
 from flask import Flask
 
 class Flytrap:
-    def __init__(self, app: Flask, config):
+    def __init__(self, config):
         self.project_id = config.get("project_id")
         self.api_endpoint = config.get("api_endpoint")
         self.api_key = config.get("api_key")
-        self.app = app
     
+
+    def setup_flask_error_handler(self, app):
+        @app.errorhandler(Exception)
+        def handle_exception(error):
+            self._log_error(error)
+            # raise error # Pass the error on to any other error handlers the user might have?
+            
